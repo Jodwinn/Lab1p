a9453c617055f5f0b5a56216cb8dde1371769dcb
Jodwinn <pavelkharkovski@gmail.com>
2025-02-23 16:01:27 +0200

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,6 @@
+number1 = float(input("Введіть перше число: "))
+number2 = float(input("Введіть друге число: "))
+
+sum = number1 + number2
+
+print(f"Сума чисел {number1} і {number2} дорівнює {sum}")
